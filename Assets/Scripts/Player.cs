2def1bc08552b11b89e7d201bc970026781f680e
alex9127git <mail.alex9127@yandex.ru>
2024-02-02 21:34:20 +0300

    Added the ability to surge
    - When the player holds Space, the cube will gradually light up with
    yellow
    - After half a second, the cube will glow
    - If player releases Space in that time, the cube will get a big boost
    in the direction it was moving. This is internally called a "Surge".
    - During a Surge, the player cannot move normally and the direction is
    locked until Surge ends

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,58 +3,104 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _accelX;
-    [SerializeField] private float _accelZ;
-    private const float _deltaAccelPerSecond = 20f;
-    private const float _accelDecayPerSecond = 0.3f;
-    private const float _maxAccel = 10f;
+    [SerializeField] private float _vx;
+    [SerializeField] private float _vz;
+    private const float _accelPerSecond = 20f;
+    private const float _decayPerSecond = 0.3f;
+    private const float _maxv = 10f;
+
+    private float _surgeChargingProcess = 0f;
+    private float _surgeX = 0f;
+    private float _surgeZ = 0f;
+    private float _surgeDecayX = 0f;
+    private float _surgeDecayZ = 0f;
+    private float _surgingProcess = 0f;
+    private bool _isSurging = false;
+
+    [SerializeField] private Renderer _renderer;
 
     private void Start()
     {
-        _accelX = 0;
-        _accelZ = 0;
+        _vx = 0;
+        _vz = 0;
     }
 
     private void Update()
     {
-        bool isAcceleratingX = false;
-        bool isAcceleratingZ = false;
-        if (Input.GetKey(KeyCode.W))
+        transform.position += new Vector3(_vx, 0, _vz) * Time.deltaTime;
+        if (_isSurging)
         {
-            _accelZ += _deltaAccelPerSecond * Time.deltaTime * Mathf.Max(1 - _accelZ / _maxAccel, 0);
-            isAcceleratingZ = true;
+            transform.position += new Vector3(_surgeX, 0, _surgeZ) * Time.deltaTime;
+            _surgeX -= _surgeDecayX * Time.deltaTime;
+            _surgeZ -= _surgeDecayZ * Time.deltaTime;
+            _surgingProcess += 2 * Time.deltaTime;
+            if (_surgingProcess >= 1f)
+            {
+                _isSurging = false;
+                _surgeX = _surgeZ = _surgeDecayX = _surgeDecayZ = 0f;
+                _surgingProcess = 0f;
+            }
         }
-        if (Input.GetKey(KeyCode.S))
+        else
         {
-            _accelZ -= _deltaAccelPerSecond * Time.deltaTime * Mathf.Max(1 + _accelZ / _maxAccel, 0);
-            isAcceleratingZ = true;
+            bool isAcceleratingX = false;
+            bool isAcceleratingZ = false;
+            if (Input.GetKey(KeyCode.W))
+            {
+                _vz += _accelPerSecond * Time.deltaTime * Mathf.Max(1 - _vz / _maxv, 0);
+                isAcceleratingZ = true;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                _vz -= _accelPerSecond * Time.deltaTime * Mathf.Max(1 + _vz / _maxv, 0);
+                isAcceleratingZ = true;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                _vx += _accelPerSecond * Time.deltaTime * Mathf.Max(1 - _vx / _maxv, 0);
+                isAcceleratingX = true;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                _vx -= _accelPerSecond * Time.deltaTime * Mathf.Max(1 + _vx / _maxv, 0);
+                isAcceleratingX = true;
+            }
+            if (!isAcceleratingX)
+            {
+                _vx *= Mathf.Pow(_decayPerSecond, Time.deltaTime);
+            }
+            if (!isAcceleratingZ)
+            {
+                _vz *= Mathf.Pow(_decayPerSecond, Time.deltaTime);
+            }
+            if (Mathf.Abs(_vx) < 0.01f)
+            {
+                _vx = 0f;
+            }
+            if (Mathf.Abs(_vz) < 0.01f)
+            {
+                _vz = 0f;
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                _surgeChargingProcess += 2 * Time.deltaTime;
+                _surgeChargingProcess = Mathf.Clamp(_surgeChargingProcess, 0, 1);
+            }
+            else
+            {
+                if (_surgeChargingProcess >= 1f && (_vx * _vx + _vz * _vz) > 1)
+                {
+                    _surgeX = _vx * 4;
+                    _surgeZ = _vz * 4;
+                    _surgeDecayX = _surgeX * 2;
+                    _surgeDecayZ = _surgeZ * 2;
+                    _surgeChargingProcess = 0f;
+                    _isSurging = true;
+                }
+                _surgeChargingProcess = 0f;
+            }
         }
-        if (Input.GetKey(KeyCode.D))
-        {
-            _accelX += _deltaAccelPerSecond * Time.deltaTime * Mathf.Max(1 - _accelX / _maxAccel, 0);
-            isAcceleratingX = true;
-        }
-        if (Input.GetKey(KeyCode.A)) 
-        { 
-            _accelX -= _deltaAccelPerSecond * Time.deltaTime * Mathf.Max(1 + _accelX / _maxAccel, 0);
-            isAcceleratingX = true;
-        }
-        if (!isAcceleratingX)
-        {
-            _accelX *= Mathf.Pow(_accelDecayPerSecond, Time.deltaTime);
-        }
-        if (!isAcceleratingZ)
-        {
-            _accelZ *= Mathf.Pow(_accelDecayPerSecond, Time.deltaTime);
-        }
-        if (Mathf.Abs(_accelX) < 0.01f)
-        {
-            _accelX = 0f;
-        }
-        if (Mathf.Abs(_accelZ) < 0.01f)
-        {
-            _accelZ = 0f;
-        }
-        transform.position += new Vector3(_accelX, 0, _accelZ) * Time.deltaTime;
+        _renderer.material.SetFloat("_Charging_Process", _surgeChargingProcess);
+        _renderer.material.SetFloat("_Surging_Process", _isSurging ? 1 - _surgingProcess : 0);
     }
 }
